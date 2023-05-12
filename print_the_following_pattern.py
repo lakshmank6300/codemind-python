1c8d58bc38a02500ccb677af1f2b8cd887455e90
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2023-05-13 00:31:00 +0530

    n=int(input()) for i in range(1,n+1):     k=1     for j in range(1,i+1):         print("%d"%(k),end="")         k+=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,11 +1,7 @@
 n=int(input())
-k=-1
 for i in range(1,n+1):
-    for j in range(i,n):
-        print(" ",end="")
-    k=k+2
-    p=k
-    while(p>0):
-        p-=1
-        print(i,end="")
+    k=1
+    for j in range(1,i+1):
+        print("%d"%(k),end="")
+        k+=1
     print()
