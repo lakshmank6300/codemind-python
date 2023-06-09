b7c4e5a7ae4196387e03f0920432f1ab38fa11b6
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2023-06-09 20:50:06 +0530

    def prime(n:int)->int:     k=int(n**0.5+1)     for i in range(2,k):         if(n%i==0):             return 0     return 1 def prev_prime(n):     while(n):         if(prime(n)):             break         else:             n-=1     return n def next_prime(n):     while(n):         if(prime(n)):             break         else:             n+=1     return n t=int(input()) for i in range(0,t):     n=int(input())     fn=prev_prime(n)     bn=next_prime(n)     if(n-fn>bn-n):         print(bn)     else:         print(fn)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,23 +1,29 @@
-def prime(n):
+def prime(n:int)->int:
     k=int(n**0.5+1)
     for i in range(2,k):
         if(n%i==0):
-            return False
-    return True
-def next_prime(n):
-    while(n):
-        if(prime(n)):
-            return n
-        n+=1
+            return 0
+    return 1
 def prev_prime(n):
     while(n):
         if(prime(n)):
-            return n
-        n-=1
+            break
+        else:
+            n-=1
+    return n
+def next_prime(n):
+    while(n):
+        if(prime(n)):
+            break
+        else:
+            n+=1
+    return n
 t=int(input())
 for i in range(0,t):
     n=int(input())
-    if(next_prime(n)-n<n-prev_prime(n)):
-        print(next_prime(n))
+    fn=prev_prime(n)
+    bn=next_prime(n)
+    if(n-fn>bn-n):
+        print(bn)
     else:
-        print(prev_prime(n))
+        print(fn)
