0ae6de0329dd599f565c5c2970e18ca1da4896c7
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2023-06-11 23:51:39 +0530

    def divsum(n):     s=0     for i in range(1,n):         if(n%i==0):             s+=i     return s a=int(input()) b=int(input()) if(divsum(a)==b and divsum(b)==a):     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,12 @@
+def divsum(n):
+    s=0
+    for i in range(1,n):
+        if(n%i==0):
+            s+=i
+    return s
 a=int(input())
 b=int(input())
-s=0
-k=0
-for i in range(1,a):
-    if(a%i==0):
-        s+=i
-for i in range(1,b):
-    if(b%i==0):
-        k+=i
-if(a==k):
+if(divsum(a)==b and divsum(b)==a):
     print("Amicable")
 else:
     print("Not Amicable")
