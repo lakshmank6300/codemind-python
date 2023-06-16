35e3c7c6fb4bd4aa5f0ff45f6a766fd1adb07cae
lakshmank6300 <119840360+lakshmank6300@users.noreply.github.com>
2023-06-16 23:01:34 +0530

    def prime(n):     k=int(n**0.5+1)     for i in range(2,k):         if(n%i==0):             return False     return True a=int(input()) b=int(input()) for i in range(a+1,b):     if(prime(i)):         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,13 +1,11 @@
-def prime(n:int)->int:
+def prime(n):
     k=int(n**0.5+1)
-    if(n==1):
-        return 0
     for i in range(2,k):
         if(n%i==0):
-            return 0
-    return 1
+            return False
+    return True
 a=int(input())
 b=int(input())
-for i in range(a,b+1):
+for i in range(a+1,b):
     if(prime(i)):
         print(i)
